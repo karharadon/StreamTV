c35776a4cf75a3238ab640b1a691b7f8def38f86
lastochkin <dmutruk.andrii.com>
2016-04-09 22:04:57 +0300

    jenkins2

---
+++ src/test/java/streamTV/BaseTest.java
@@ -0,0 +1,42 @@
+package streamTV;
+
+import helper.ConfigProperties;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import pages.HomePage;
+
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class BaseTest {
+
+    public static WebDriver driver;
+
+    protected String browser = ConfigProperties.getProperty("browser");
+    HomePage homePage = PageFactory.initElements(getWebDriver(), HomePage.class);
+
+
+    public WebDriver getWebDriver() {
+        if (browser.equals("chrome")) {
+            driver = new ChromeDriver();
+        }
+        if (browser.equals("firefox")) {
+            driver = new FirefoxDriver();
+        }
+        if (browser.equals("internetExplorer")) {
+            driver = new InternetExplorerDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(Long.parseLong(ConfigProperties.getProperty("impWait")), TimeUnit.SECONDS);
+        return driver;
+    }
+
+   //@AfterMethod
+    public void deleteWrestlersAndTearDown() {
+       homePage.deleteAllMyWrestlers();
+       driver.close();
+    }
+}
---
+++ src/test/java/streamTV/RegisterTest.java
@@ -0,0 +1,57 @@
+package streamTV;
+
+import org.testng.annotations.Test;
+import streamTV.BaseTest;
+
+public class RegisterTest extends BaseTest {
+
+    @Test
+    public void createAndDelete(){
+        homePage.login(driver);
+        homePage.createWrestler(homePage.wrestler1);
+        homePage.findWrestler(homePage.wrestlerFullName1);
+        homePage.deleteWrestler(homePage.wrestlerFullName1);
+        homePage.checkDeletion(homePage.wrestlerFullName1);
+    }
+
+    @Test
+    public void createAndVerify(){
+        homePage.login(driver);
+        homePage.createWrestler(homePage.wrestler1);
+        homePage.findWrestler(homePage.wrestlerFullName1);
+        homePage.verifySearchResultWithCode(homePage.wrestler1, homePage.wrestlerFullName1);
+        homePage.verifyProfileDataWithCode(homePage.wrestler1);
+        homePage.deleteWrestler(homePage.wrestlerFullName1);
+        homePage.checkExeptions();
+    }
+
+    @Test
+    public void updateAndVerify(){
+        homePage.login(driver);
+        homePage.createWrestler(homePage.wrestler1);
+        homePage.updateWrestler();
+        homePage.findWrestler(homePage.wrestlerFullName2);
+        homePage.verifySearchResultWithCode(homePage.wrestler2, homePage.wrestlerFullName2);
+        homePage.verifyProfileDataWithCode(homePage.wrestler2);
+        homePage.deleteWrestler(homePage.wrestlerFullName2);
+        homePage.checkExeptions();
+    }
+
+    @Test
+    public void Filters(){
+        homePage.login(driver);
+        homePage.createFewWrestlersForTestingFilters();
+        homePage.useAndCheckDifferentFilters();
+        homePage.deleteWrestlersCreatedForTestingFilters();
+    }
+
+
+    @Test
+    public void Filtersssss(){
+        homePage.login(driver);
+        homePage.createFewWrestlersForTestingFilters();
+        homePage.useFilters();
+        homePage.checkAllFilters();
+        homePage.deleteWrestlersCreatedForTestingFilters();
+    }
+}
